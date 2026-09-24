3690d439d92b57f79d4df6c57d59db78b835d135
Tai Groot <tai@taigrr.com>
2026-09-24 06:34:10 +0000

    fix(models): propagate glTF node errors

--- pkg/models/gltf_test.go
+++ pkg/models/gltf_test.go
@@ -1,7 +1,11 @@
 package models
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/qmuntal/gltf"
+	"github.com/taigrr/trophy/pkg/math3d"
 )
 
 func TestLoadGLBInvalidPath(t *testing.T) {
@@ -22,5 +26,43 @@ func TestGLTFLoaderCreation(t *testing.T) {
 	}
 	if !loader.SmoothNormals {
 		t.Error("SmoothNormals should default to true")
+	}
+}
+
+func TestGLTFProcessNodePropagatesMeshErrors(t *testing.T) {
+	loader := NewGLTFLoader()
+	doc := &gltf.Document{
+		Accessors: []*gltf.Accessor{
+			{
+				BufferView:    gltf.Index(0),
+				ComponentType: gltf.ComponentFloat,
+				Count:         1,
+				Type:          gltf.AccessorScalar,
+			},
+		},
+		Meshes: []*gltf.Mesh{
+			{
+				Primitives: []*gltf.Primitive{
+					{
+						Attributes: gltf.PrimitiveAttributes{
+							gltf.POSITION: 0,
+						},
+					},
+				},
+			},
+		},
+		Nodes: []*gltf.Node{
+			{
+				Mesh: gltf.Index(0),
+			},
+		},
+	}
+
+	err := loader.processNode(doc, 0, math3d.Identity(), NewMesh("bad"), map[int]bool{})
+	if err == nil {
+		t.Fatal("expected invalid POSITION accessor error")
+	}
+	if !strings.Contains(err.Error(), "expected VEC3") {
+		t.Fatalf("expected VEC3 error, got %v", err)
 	}
 }
